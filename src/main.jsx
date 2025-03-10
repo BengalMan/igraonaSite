db16d595dda602ad6699ef2061340fb447886dff
BengalMan <jakovvukovic557@gmail.com>
2025-03-10 14:38:28 +0100

    Sixth commit
    10.3.2025 14:38

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider, } from "react-router-dom";
-import SignUpScreen, { SignUpScreenAction } from './SignUpScreen.jsx';
+import SignUpScreen from './SignUpScreen.jsx';
 
 const router = createBrowserRouter([
   {
@@ -11,9 +11,8 @@ const router = createBrowserRouter([
     element: <App />,
   },
   {
-    path: "/signUp",
+    path: "/tt4",
     element: <SignUpScreen />,
-    action: SignUpScreenAction
   },
 ]);
 
